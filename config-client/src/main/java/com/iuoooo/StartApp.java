ab67bb2e2699acab8b17b823a165325d77b4d937
iuoooo <javalijingkui@163.com>
2020-01-16 14:19:42 +0800

    将配置服务 注册到euraka注册中心，并是现实远程git读取文件

--- config-client/src/main/java/com/iuoooo/StartApp.java
+++ config-client/src/main/java/com/iuoooo/StartApp.java
@@ -2,8 +2,10 @@ package com.iuoooo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StartApp {
     public static void main(String[] args) {
         SpringApplication.run(StartApp.class, args);
